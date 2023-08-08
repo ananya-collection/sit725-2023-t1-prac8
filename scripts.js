22eea81c11f91cae5840a6576d9b15684510a5ba
ananyaKrishnan-2023 <s222625051@deakin.edu.au>
2023-08-08 22:04:13 +1000

    Add a DB
    Successfully closed.

--- scripts.js
+++ scripts.js
@@ -1,51 +1,57 @@
-const cardList = [{
-    title: 'cat 2',
-    path: 'images/kitten1.jpg',
-    subTitle: 'About Cat 2',
-    description: 'Description of Cat 2'
-},
-{
-    title: 'cat 3',
-    path: 'images/kitten2.jpg',
-    subTitle: 'About Cat 3',
-    description: 'Description of Cat 3'
-},
-{
-    title: 'cat 4',
-    path: 'images/kitten3.jpg',
-    subTitle: 'About Cat 4',
-    description: 'Description of Cat 4'
-}];
-
+//card's display format
 const addCards = (items) => {
     items.forEach(item => {
         let itemToAppend = '<div class="col s4 center-align">' +
-            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.path + '">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.Path + '">' +
             '</div><div class="card-content">' +
-            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.subTitle + '</a></p></div>' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.Title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.SubTitle + '</a></p></div>' +
             '<div class="card-reveal">' +
-            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
-            '<p class="card-text">' + item.description + '</p>' +
+            '<span class="card-title grey-text text-darken-4">' + item.Title + '<i class="material-icons right">close</i></span>' +
+            '<p class="card-text">' + item.Description + '</p>' +
             '</div></div></div>';
         $("#card-section").append(itemToAppend)
     });
 }
-
-const formSumitted = () => {
+//the form for posting data
+const formSubmitted = () => {
     let formData = {};
-    formData.firstName = $('#first_name').val();
-    formData.lastName = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    formData.Title = $('#Title').val();
+    formData.SubTitle = $('#SubTitle').val();
+    formData.Path = $('#Path').val();
+    formData.Description = $('#Description').val();
 
     console.log(formData);
+    console.log(catPost);
+    catPost(formData);
+    
+}
+
+function catPost(cat){
+    $.ajax({
+        url:'/api/cat',
+        type:'POST',
+        data:cat,
+        success: (result)=>{
+            if(result.statusCode == 201){
+                alert('cat post successful');
+            }
+        }
+    });
+}
+
+function getAllCats(){
+    $.get('/api/cats', (response)=>{
+        if(response.statusCode == 200){
+        addCards(response.data);
+        }
+    });
 }
 
 $(document).ready(function(){
     $('.materialboxed').materialbox();
     $('#formSubmit').click(()=>{
-        formSumitted();
+        formSubmitted();
     });
-    addCards(cardList);
     $('.modal').modal();
+    getAllCats();
 });
--- server.js
+++ server.js
@@ -1,28 +1,79 @@
-//// Import the express library
+//import the express library
 let express = require('express');
+//import mongodb using MongpClient
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://s222625051:user00@cluster0.2rxiut4.mongodb.net/?retryWrites=true&w=majority";
+//db collection declared
+let collection;
 
-// // Initialize an express application
+//initialize an express application
 let app = express();
-
+//set the server port. Initially, it tries to use the PORT environment variable value,but if it doesn't exist/undefined, it uses 3000 by default as the port
+let port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
 
-// Set the server port. First, it tries to use the PORT environment variable value, 
-// but if it doesn't exist (undefined), it uses 3000 as a default port
-let port = process.env.PORT || 3000;
+//create a MongoClient to set up the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function runDBConnection() {
+  try {
+    await client.connect();
+    collection= client.db().collection('Cat');
+    console.log('collection');
+    //console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  catch(ex) {
+    console.error(ex);
+  }
 
-// Serve static files (like HTML, CSS, images) from the current directory
+}
+
+//by using the current directory, static files (like HTML, CSS, images)are served
 app.use(express.static(__dirname + '/'));
 
-// Handle GET requests to the server root ('/') by rendering 'index.html'
-// (this assumes that a view engine is set up)
-app.get('/', (req, res) => {
+//by rendering the index html file a view engine is set up
+app.get('/', (_, res) => {
     res.render('index.html');
 });
 
+//get fn in the back-end
+app.get('/api/cats', (req,res) => {
+  getAllCats((err, result)=> {
+    if(!err){
+      res.json({statusCode:200, data:result, message:'get all cats success'});
+    }
+  })
+ }
+)
+
+//post fn in the back-end
+app.post('/api/cat', (req,res)=>{
+    let cat = req.body;
+    catPost(cat, (err, result)=>{
+      if(!err){
+        res.json({statusCode:201, data:result, message:'success'});
+      }
+
+    });
+});
+
+function getAllCats(callback){
+  collection.find({}).toArray(callback);
+}
+
+function catPost(cat, callback){
+    collection.insertOne(cat, callback);
+}
 app.listen(port, () => {
     console.log('server started');
+    runDBConnection().catch(console.dir);
 });
